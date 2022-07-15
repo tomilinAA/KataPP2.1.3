25e4480dae5f23648c607530a64e63fb1f3bc4a2
tomilinAA <tomilin-2000@mail.ru>
2022-07-15 12:16:01 +0300

    attempt 1

--- src/main/java/app/model/Timer.java
+++ src/main/java/app/model/Timer.java
@@ -1,8 +1,11 @@
 package app.model;
 
-public class Timer {
+import org.springframework.stereotype.Component;
 
-    private Long nanoTime = System.nanoTime();
+@Component
+public  class Timer {
+
+    private final Long nanoTime = System.nanoTime();
 
     public Long getTime() {
         return nanoTime;
